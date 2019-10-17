9981693da1c4b462b6f4dacb323f95f87aa0f5d9
Ethan Chang <ethanc@usc.edu>
2019-10-17 00:38:21 -0700

    adding aws amplify to project and testing with s3

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
+import Amplify from 'aws-amplify' 
+import config from './aws-exports'
+
+
+//amplify configuration
+Amplify.configure(config);
 
 AppRegistry.registerComponent(appName, () => App);
